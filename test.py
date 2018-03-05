c6842fc51a71ce4c6bf6ef69090515c5ee0da70b
linbo <1434442536@qq.com>
2018-03-05 17:23:20 +0800

    测试文件

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from max_min_cluster import max_min_cluster
 from knn_cluster import knn_cluster
 from hierarchical_cluster import hierarchical_cluster
 from k_means_cluster import k_means_cluster
-
+from isodata_cluster import IsoData
 
 # data = [[0, 0], [3, 8], [1, 1], [2, 2], [5, 3], [4, 8], [6, 3], [5, 4], [6, 4], [7, 5]]
 
@@ -24,11 +24,17 @@ from k_means_cluster import k_means_cluster
 # result = hierarchical_cluster(data, t)
 
 # 测试k_means_cluster
-data = [[0, 0], [1, 0], [0, 1], [1, 1], [2, 1], [1, 2], [2, 2], [3, 2], [6, 6], [7, 6], [8, 6], [6, 7], [7, 7], [8, 7],
-        [9, 7], [7, 8], [8, 8], [9, 8], [8, 9], [9, 9]]
-k = 2
-result = k_means_cluster(data, k)
+# data = [[0, 0], [1, 0], [0, 1], [1, 1], [2, 1], [1, 2], [2, 2], [3, 2], [6, 6], [7, 6], [8, 6], [6, 7], [7, 7],
+#         [8, 7], [9, 7], [7, 8], [8, 8], [9, 8], [8, 9], [9, 9]]
+# k = 2
+# result = k_means_cluster(data, k)
 
+# 测试ISODATA
+data = [[0, 0], [1, 1], [2, 2], [4, 3], [5, 3], [4, 4], [5, 4], [6, 5]]
+initial_set = [1, 2, 1, 1, 4, 0, 4, 0.5]
+
+isodata = IsoData(initial_set, data)
+result = isodata.result
 
 for i in range(len(result)):
     print "----------第" + str(i+1) + "个聚类----------"
